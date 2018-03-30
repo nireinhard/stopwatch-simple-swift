1b0064f1f2d8767de97241c2b2dccceb48dabb8a
nireinhard <nireinhard@htwsaar.de>
2018-03-30 22:44:59 +0200

    Initial Commit

---
+++ Stoppuhr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stoppuhr
+//
+//  Created by Niklas Reinhard on 30.03.18.
+//  Copyright © 2018 Niklas Reinhard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
